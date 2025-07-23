93e4ef2dcea7f6b33d020e8bffaf8f5ee60e51d8
Mansi Verma <mansi09verma@gmail.com>
2025-07-23 17:46:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(f"🥤Customize Your Smoothies!🥤")
@@ -28,6 +29,10 @@ if ingredients_list:
     # st.text(ingredients_list)
 
     ingredients_string = ''
+    smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    # st.text(smoothiefroot_response.json())
+    sf_df = st.dataframe(data_smoothiefroot_response.json(), use_container_width = True)
+
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
@@ -47,6 +52,4 @@ if ingredients_list:
 
         st.success(f"Your Smoothie is ordered, {name_on_order}!" , icon="✅")
 
-import requests
-smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+
