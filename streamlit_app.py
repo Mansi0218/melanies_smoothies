6e490e5ff83c506518f84f3db699940e130116cd
Mansi Verma <mansi09verma@gmail.com>
2025-07-23 17:15:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ ingredients_list = st.multiselect (
     , max_selections=5
 )
 if ingredients_list:
-    st.write(ingredients_list)
+    # st.write(ingredients_list)
     st.text(ingredients_list)
 
     ingredients_string = ''
